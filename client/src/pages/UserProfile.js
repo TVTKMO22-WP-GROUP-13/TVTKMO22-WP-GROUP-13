f508d7dbdf91c547babe742d3e5404aad802c034
yuudachi13 <t2pupa00@students.oamk.fi>
2024-04-02 17:50:48 +0300

    Alustettu itelle UserProfile

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -3,6 +3,20 @@ import React from 'react'
 
 export default function UserProfile() {
   return (
-    <div>UserProfile</div>
+    <>
+    <div className='Konkka'>
+    <div className='KolmeEkaa'>
+    <div className='Kaikki' id='UserProfileAndAva'>UserProfile</div>
+    <div className='Kaikki' id='ReviewsDiv'> Tänne tulee userin revit</div>
+    <div className='Kaikki' id='Listat'> tänne tulee listat mitä on katottu yms</div>
+    </div>
+    <div className='ToisetKolme'>
+    <div className='Kaikki' id='Favourites'> tänne tulee käyttäjän favs</div>
+    <div className='Kaikki' id='LastWatched'> Tänne tulee mitä on viimeks katottu</div>
+    <div className='Kaikki' id='Groups'> ja tänne tulee käyttäjän groupit</div>
+    </div>
+    </div>
+    </>
   )
+
 }
