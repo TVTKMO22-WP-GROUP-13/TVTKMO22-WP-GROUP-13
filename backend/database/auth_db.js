7f54ad3b0fb76cc687bbe6af2f54ca668e293a06
JoonasSal <122926329+JoonasSal@users.noreply.github.com>
2024-04-08 00:59:28 +0300

    REST user groupin luominen ja kaikkien grouppien hakeminen

--- backend/database/auth_db.js
+++ backend/database/auth_db.js
@@ -2,7 +2,8 @@ const pgPool = require('./pg_connection');
 
 const sql = {
     REGISTER: 'INSERT INTO user_data (username, password_hash) VALUES ($1, $2)',
-    GET_PASSWORD: 'SELECT password_hash FROM user_data WHERE username=$1'
+    GET_PASSWORD: 'SELECT password_hash FROM user_data WHERE username=$1',
+    CREATE_GROUP: 'INSERT INTO user_group (group_name, description, owner_id) VALUES ($1, $2, $3)',
 };
 
 async function register(username, password_hash){
@@ -15,4 +16,8 @@ async function getPassword(username){
 
 }
 
-module.exports = {register, getPassword};
+async function createGroup(group_name, description, owner_id){
+    await pgPool.query(sql.CREATE_GROUP, [group_name, description, owner_id]);
+}
+
+module.exports = {register, getPassword, createGroup};
