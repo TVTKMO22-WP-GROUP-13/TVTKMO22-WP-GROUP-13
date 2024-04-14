871299bdb734ef5c809924d7e289eee151544193
JoonasSal <122926329+JoonasSal@users.noreply.github.com>
2024-04-14 21:51:58 +0300

    poistettu ominaisuus joka näyttää ryhmän omistajan group search sivulla

--- client/src/pages/GroupSearch.js
+++ client/src/pages/GroupSearch.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import './GroupSearch.css'
 
 function GroupSearch() {
-  const [allGroups, setAllGroups] = useState([]);
   const [displayedGroups, setDisplayedGroups] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
 
@@ -10,22 +9,18 @@ function GroupSearch() {
     const fetchGroups = async () => {
       const response = await fetch('http://localhost:3001/user_group/all');
       const data = await response.json();
-      const groupsWithOwnerNames = await Promise.all(data.groups.map(async group => {
-        const ownerResponse = await fetch(`http://localhost:3001/user_data/user_id?user_id=${group.owner_id}`);
-        const ownerData = await ownerResponse.json();
-        return { ...group, owner_name: ownerData.user.username };
-      }));
-      setAllGroups(groupsWithOwnerNames);
-      setDisplayedGroups(groupsWithOwnerNames);
+      setDisplayedGroups(data.groups);
     };
   
     fetchGroups();
   }, []);
 
   useEffect(() => {
-    const filteredGroups = allGroups.filter(group => group.group_name.toLowerCase().includes(searchTerm.toLowerCase()));
+    const filteredGroups = displayedGroups.filter(group => 
+      group.group_name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
     setDisplayedGroups(filteredGroups);
-  }, [searchTerm, allGroups]);
+  }, [searchTerm]);
 
   return (
     <div className="group-list">
@@ -35,7 +30,6 @@ function GroupSearch() {
       {displayedGroups.map((group) => (
         <div className="group-list-item" key={group.group_id}>
           <h2>{group.group_name}</h2>
-          <p>Created by: {group.owner_name}</p>
           <p>Description: {group.description}</p>
         </div>
       ))}
