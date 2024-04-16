2a488d55a0a05ffff969531330098502d56b6c22
Hertsi <nico.hertolin@protonmail.com>
2024-04-16 19:21:12 +0300

    series alottelut
    series alottelut

--- backend/tmdb/tmdb_endpoints.js
+++ backend/tmdb/tmdb_endpoints.js
@@ -5,6 +5,13 @@ const tmdbEndpoints = {
     `/discover/movie?with_genres=${genreId}&sort_by=${sort_by}&page=${page}&primary_release_year=${year}&language=${language}`,
   getMovieById: (id) => `/movie/${id}`
   //getMovieProvidersById: (id) => `/movie/${id}/watch/providers`,
+  //router.get('/media', mediaService.searchMedia);
+
+//router.get('/series/search', movieService.searchTvShows);
+
+//router.get('/series/discover', movieService.discoverTvShows);
+
+//router.get('/series/:id', movieService.getTvShowById);
 };
 
 module.exports = tmdbEndpoints;
