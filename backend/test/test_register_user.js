25ca289753d01149d7c08ce3503b0dccda99a362
JoonasSal <122926329+JoonasSal@users.noreply.github.com>
2024-04-28 01:02:17 +0300

    testing testailua

--- backend/test/test_register_user.js
+++ backend/test/test_register_user.js
@@ -3,6 +3,7 @@ const chaiHttp = require('chai-http');
 const server = require('../app');
 const sinon = require('sinon');
 const bcrypt = require('bcrypt');
+const { faker } = require('@faker-js/faker');
 
 chai.use(chaiHttp);
 const expect = chai.expect;
@@ -18,10 +19,13 @@ describe('POST /register', () => {
         sinon.restore();
     });
 
+    const username = faker.internet.userName();
+    const password = faker.internet.password(8);
+
     it('should register a user successfully', (done) => {
         chai.request(server)
             .post('/authentication/register')
-            .send({ username: 'newUser', pw: 'password' })
+            .send({ username: username, pw: password })
             .end((err, res) => {
                 expect(res).to.have.status(201);
                 expect(res.body).to.have.property('message', 'User registered successfully');
@@ -32,7 +36,7 @@ describe('POST /register', () => {
     it('should reject registration with a username that is too short', (done) => {
         chai.request(server)
             .post('/authentication/register')
-            .send({ username: 'abc', pw: 'password123' })
+            .send({ username: 'abc', pw: password })
             .end((err, res) => {
                 expect(res).to.have.status(400);
                 expect(res.body).to.have.property('error', 'Username must be at least 4 characters long');
@@ -43,7 +47,7 @@ describe('POST /register', () => {
     it('should reject registration with a password that is too short', (done) => {
         chai.request(server)
             .post('/authentication/register')
-            .send({ username: 'newUser', pw: 'abc' })
+            .send({ username: username, pw: 'abc' })
             .end((err, res) => {
                 expect(res).to.have.status(400);
                 expect(res.body).to.have.property('error', 'Password must be at least 4 characters long');
@@ -52,11 +56,9 @@ describe('POST /register', () => {
     });
 
     it('should reject registration if username is already taken', (done) => {
-        const error = new Error('Duplicate username');
-        error.code = '23505';
         chai.request(server)
             .post('/authentication/register')
-            .send({ username: 'newUser', pw: 'password' })
+            .send({ username: username, pw: password })
             .end((err, res) => {
                 expect(res).to.have.status(409);
                 expect(res.body).to.have.property('error', 'Username is already taken');
