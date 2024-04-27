1e8672dc936e68d4fdfc19d9daa97f5c4d648236
cheezyx <cheezyx@users.noreply.github.com>
2024-04-27 23:10:35 +0300

    Add favorites / groupmedia napit (ei oo nätti mut ne toimii)

--- client/src/pages/MovieSearch.js
+++ client/src/pages/MovieSearch.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './MovieSearch.css';
+import { jwtToken } from '../components/AuSignal';
 
 const genresList = [
   'action', 'adventure', 'animation', 'comedy', 'crime', 'documentary',
@@ -36,8 +37,83 @@ const genreIdsMap = {
   "talk": 10767,
   "war & politics": 10786
 };
-
 const MovieResults = ({ movies }) => {
+  const [Choice, SetChoice] = useState('');
+  const [createdGroups, setCreatedGroups] = useState([]);
+  const [joinedGroups, setJoinedGroups] = useState([]);
+  const [showChoices, setshowChoices] = useState(false);
+
+  useEffect(() => {
+    const GetGroups = async (url, setUserGroups, groupType) => {
+      //console.log(`Get ${groupType} groups`)
+      try {
+        const response = await axios.get(url, {
+          headers: {
+            'Authorization': `Bearer ${jwtToken.value}`
+          }
+        })
+        //console.log(`${groupType} groups`, response.data)
+        const groupData = groupType === 'created' ? response.data.groups : response.data.groupsJoined
+        if (groupData && groupData.length > 0) {
+          setUserGroups(groupData)
+         
+        } else {
+          console.log(`No ${groupType} groups found`)
+        }    
+      } catch (error) {
+        console.error(`Error fetching ${groupType} groups:`, error)
+      }
+    }
+   
+    if(jwtToken.value) {
+      GetGroups('http://localhost:3001/user_group/getUserCreatedGroups', setCreatedGroups, 'created');
+      GetGroups('http://localhost:3001/group_member/groups_joined', setJoinedGroups, 'joined');
+      setshowChoices(true)
+    } else {
+      console.log("no token")
+      setshowChoices(false)
+     
+    }
+  }, [])
+  const handleAdd = async (movieData, Choice) => {
+    const { id } = movieData
+        console.log("SelectedMovie:", id)
+        console.log("SelectedChoice:", Choice)
+        if(Choice === 'Favorite') {
+        console.log("Add to favorites")
+            axios.post(`http://localhost:3001/list_entry/addEntry`, {
+            tmdb_id: id,
+            media_type: 'movie',
+            list_type: 'Favorite'
+          }, 
+          {
+            headers: {
+              'Authorization': `Bearer ${jwtToken.value}`
+            }
+          }).then(response => {
+            console.log("Success:", response.data.message)
+          } )
+        } else if (Choice === 'Select'){
+          console.log("No nothing")
+          alert("Please select a choice before adding")
+        }
+        else {
+          console.log("Add to group")
+          axios.post(`http://localhost:3001/list_entry/addEntry`, {
+            tmdb_id: id,
+            media_type: 'movie',
+            list_type: 'GroupMedia',
+            group_id: Choice
+          }, {
+            headers: {
+              'Authorization': `Bearer ${jwtToken.value}`
+            }
+          }).then(response => {
+            console.log("Success:", response.data.message)
+          } )
+        }
+      }
+
   return (
     <div className="movie-results-wrapper">
       {movies.map((movie) => (
@@ -47,11 +123,27 @@ const MovieResults = ({ movies }) => {
             <h3>{movie.title}</h3>
             <p>{movie.overview}</p>
           </div>
+          <div>
+            {showChoices && (     
+            <><select value={Choice} onChange={(e) => SetChoice(e.target.value)}>
+            <option value="Select">Select</option>
+            <option value="Favorite">Favorite</option>
+            {createdGroups.map(group => (
+              <option key={group.group_id} value={group.group_id}>{group.group_name}</option>
+            ))}
+            {joinedGroups.map(group => (
+              <option key={group.group_id} value={group.group_id}>{group.group_name}</option>
+            ))}
+            </select><button onClick={() => handleAdd(movie, Choice)}>Add</button></>
+            )} 
+          </div>
+
         </div>
       ))}
     </div>
   );
 };
+
 
 const Movies = () => {
   const [query, setQuery] = useState('');
@@ -65,6 +157,11 @@ const Movies = () => {
     const genreIds = Array.from(selectedGenres).map(genre => genreIdsMap[genre]).join(',');
     setGenreQuery(genreIds);
   }, [selectedGenres]);
+
+  useEffect(() => {
+    handleSearch();
+  }, [page]);
+
 
   const handleSearch = async () => {
     try {
@@ -116,6 +213,7 @@ const Movies = () => {
     setYear(event.target.value);
   };
 
+
   return (
     <div className="main-container">
       <div className="search-wrapper">
@@ -154,8 +252,8 @@ const Movies = () => {
 
         <button className="search-button" onClick={handleSearch}>Search</button>
       </div>
-
-      <MovieResults movies={movies} />
+      <MovieResults movies={movies}/>
+         
     </div>
   );
 };
